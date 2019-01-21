761d396311235763b93f8d55ccb76441549f997a
JensMellberg <dat14jme@student.lu.se>
2019-01-21 16:09:13 +0100

    shitty first version

---
+++ Controllers/MacStats.js
@@ -0,0 +1,11 @@
+function chooseAll() {
+  var obj = document.getElementById('country')
+  for (var i=0; i<obj.options.length; i++) {
+    obj.options[i].selected = true;
+    }
+
+}
+
+function drawStatistic() {
+
+}
