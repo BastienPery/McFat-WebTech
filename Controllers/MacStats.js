68ec205f46f2b3013e9b27d0ff377b5055b3f2cd
BastienPery <bastien.pery@gmail.com>
2019-02-01 11:57:20 +0100

    Canvas modifications

--- Controllers/MacStats.js
+++ Controllers/MacStats.js
@@ -9,59 +9,59 @@ var datapoints = []
 
 function chooseAll() {
   var obj = document.getElementById('countries')
-  for (var i=0; i<obj.options.length; i++) {
+  for (var i = 0; i < obj.options.length; i++) {
     obj.options[i].selected = true;
-    }
+  }
 
 }
 
 function drawStatistic() {
-  if($("#stats option:selected").val() == "nbr") {
-      NbrOfMcDonalds()
+  if ($("#stats option:selected").val() == "nbr") {
+    NbrOfMcDonalds()
   }
-  else if($("#stats option:selected").val() == "nbrPC") {
+  else if ($("#stats option:selected").val() == "nbrPC") {
     NbrOfMcDonaldsPC()
-}
+  }
 }
 
 function NbrOfMcDonalds() {
 
   var currentdata = [];
   var obj = document.getElementById('countries')
-  for (var i=0; i<obj.options.length; i++) {
+  for (var i = 0; i < obj.options.length; i++) {
     if (obj.options[i].selected) {
       var country = obj.options[i].innerHTML;
-      var data = {y: datapoints[country].mc, label: country}
+      var data = { y: datapoints[country].mc, label: country }
       currentdata.push(data)
     }
-    }
+  }
 
-    drawBarChart("Number of McDonalds per country", "Number of McDonalds", currentdata)
+  drawBarChart("Number of McDonalds per country", "Number of McDonalds", currentdata)
 }
 
 function NbrOfMcDonaldsPC() {
 
   var currentdata = [];
   var obj = document.getElementById('countries')
-  for (var i=0; i<obj.options.length; i++) {
+  for (var i = 0; i < obj.options.length; i++) {
     if (obj.options[i].selected) {
       var country = obj.options[i].innerHTML;
-      var data = {y: datapoints[country].mc/(datapoints[country].pop/100000), label: country}
+      var data = { y: datapoints[country].mc / (datapoints[country].pop / 100000), label: country }
       currentdata.push(data)
     }
-    }
+  }
 
-    drawBarChart("Number of McDonalds per 100 000 people", "Number of McDonalds per 100 000 people", currentdata)
+  drawBarChart("Number of McDonalds per 100 000 people", "Number of McDonalds per 100 000 people", currentdata)
 }
 
 
 function onLoad() {
-    $.ajax({
-        type: "GET",
-        url: "McDonaldsPerCountry.txt",
-        dataType: "text",
-        success: function(data) {processData(data);}
-});
+  $.ajax({
+    type: "GET",
+    url: "McDonaldsPerCountry.txt",
+    dataType: "text",
+    success: function (data) { processData(data); }
+  });
 }
 
 function processData(allText) {
@@ -69,55 +69,51 @@ function processData(allText) {
   var select = document.getElementById('countries');
 
 
-    var allTextLines = allText.split(/\r\n|\n/);
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(",");
-        if (data[0] == "")
-        {
-          continue;
-        }
-          datapoints[data[0]] = {mc:parseFloat(data[1]), pop:parseFloat(data[2])};
-        var opt = document.createElement('option');
-        opt.value = i;
-        opt.innerHTML = data[0];
-        select.appendChild(opt);
+  var allTextLines = allText.split(/\r\n|\n/);
+  for (var i = 1; i < allTextLines.length; i++) {
+    var data = allTextLines[i].split(",");
+    if (data[0] == "") {
+      continue;
     }
+    datapoints[data[0]] = { mc: parseFloat(data[1]), pop: parseFloat(data[2]) };
+    var opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = data[0];
+    select.appendChild(opt);
+  }
 
-    var options = $("#countries option");                    // Collect options
-options.detach().sort(function(a,b) {               // Detach from select, then Sort
+  var options = $("#countries option");                    // Collect options
+  options.detach().sort(function (a, b) {               // Detach from select, then Sort
     var at = $(a).text();
     var bt = $(b).text();
-    return (at > bt)?1:((at < bt)?-1:0);            // Tell the sort function how to order
-});
-options.appendTo("#countries");
+    return (at > bt) ? 1 : ((at < bt) ? -1 : 0);            // Tell the sort function how to order
+  });
+  options.appendTo("#countries");
 
 }
 
 function drawBarChart(title, text, labels) {
   var canvas = document.getElementById('myCanvas');
-// check for browser support
-if (canvas && canvas.getContext) {
-var ctx = canvas.getContext('2d');
-if (ctx) {
-  ctx.fillStyle = '#000';
+  // check for browser support
+  if (canvas && canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    if (ctx) {
+      ctx.fillStyle = '#000';
     }
-}
-canvas.width = 1000;
-canvas.height = 1000;
-ctx.font = "20px Arial";
-ctx.textAlign = "end"
-max = 0;
-maxDist = 600;
-for (i = 0; i < labels.length; i++) {
-  ctx.fillText(labels[i].label, 220, 20+20*i);
-  max = Math.max(max, labels[i].y);
-}
+  }
+  canvas.width = 1000;
+  canvas.height = 1000;
+  ctx.font = "20px Arial";
+  ctx.textAlign = "end"
+  max = 0;
+  maxDist = 700;
+  for (i = 0; i < labels.length; i++) {
+    ctx.fillText(labels[i].label, 235, 20 + 30 * i);
+    max = Math.max(max, labels[i].y);
+  }
   ctx.fillStyle = '#F00';
-for (i = 0; i < labels.length; i++) {
-  ctx.fillRect(230, 10+20*i, (labels[i].y/max)*maxDist,8);
-}
-
-
-
-
+  for (i = 0; i < labels.length; i++) {
+    ctx.fillRect(245, 10 + 30 * i, (labels[i].y / max) * maxDist, 10);
+    ctx.fillText(labels[i].y, 295 + (labels[i].y / max) * maxDist, 20 + 30 * i);
+  }
 }
